4391484649efb4603616c523a7a5514cd00a53b0
trallarn <trallarn@yahoo.com>
2016-05-30 23:23:19 +0200

    charts data from local

--- src/js/chart.js
+++ src/js/chart.js
@@ -1,6 +1,27 @@
 var Highcharts = require('highcharts/highstock');
+var $ = require('jquery');
 
 // Create the chart
-Highcharts.chart('chart', {
-    /*Highcharts options*/ 
+//$.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=aapl-c.json&callback=?', function (data) {
+$.getJSON('http://localhost:3000/daily/eric?chartType=ohlc&callback=?', function (data) {
+    Highcharts.StockChart('chart',  {
+        rangeSelector : {
+            selected : 1
+        },
+
+        title : {
+            text : data.symbol + ' Stock Price'
+        },
+
+        series : [{
+            type: 'candlestick',
+            //type: 'line',
+            name : data.symbol,
+            data : data.quotes,
+            tooltip: {
+                valueDecimals: 2
+            }
+        }]
+    });
 });
+
