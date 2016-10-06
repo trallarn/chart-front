f8f7e69499c370da9822ebb6979b9a2141d4c7b5
trallarn <trallarn@yahoo.com>
2016-10-06 14:10:03 +0200

    adds state to instruments list

--- src/js/components/chartAndList.js
+++ src/js/components/chartAndList.js
@@ -2,11 +2,38 @@ var $ = require('jquery');
 var _ = require('underscore');
 var ko = require('knockout');
 
+var stateRW = require('../infrastructure/StateRW');
+
 ko.components.register('chartAndList', {
     viewModel: function(params) {
+        var self = this;
+        this.stateId = 'chartAndList';
+
         this.chartedInstrument = ko.observable();
         this.comparedInstruments = ko.observableArray();
-        this.currentMenuItem = ko.observable('WinnerLoser');
+        this.currentMenuItem = ko.observable();
+
+        this.currentMenuItem.subscribe(function() {
+            self.saveState();
+        });
+
+        self.saveState = function() {
+            stateRW.save(self.stateId, {
+                currentMenuItem: self.currentMenuItem()
+            });
+        };
+
+        self.loadState = function() {
+            var state = stateRW.read(self.stateId);
+            
+            if(state) {
+                this.currentMenuItem(state.currentMenuItem);
+            } else {
+                this.currentMenuItem('Instruments');
+            } 
+        };
+
+        self.loadState();
     },
     template: require('../templates/chartAndList.html')
 });
